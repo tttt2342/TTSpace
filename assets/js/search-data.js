041dc33f9fbc34f7eff18e60d3d0bfc00767d630
tttt2342 <37260130+tttt2342@users.noreply.github.com>
2024-12-25 13:56:59 +0000

    Deploying to gh-pages from @ tttt2342/TTSpace@8471b973357116ebda9d4f466a1798e0b3380b8e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-physics-experiments-course-presentation-has-been-succesfully-completed-there-is-no-experiment-course-anymore",
+          title: 'Physics Experiments Course presentation has been succesfully completed. There is no experiment course...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
