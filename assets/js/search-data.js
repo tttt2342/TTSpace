807e179e4c66a621becf2a3e3a86e05e74ea5a46
tttt2342 <37260130+tttt2342@users.noreply.github.com>
2024-12-29 17:32:24 +0000

    Deploying to gh-pages from @ tttt2342/TTSpace@f9b147a956a05d5cc98bb94913426a198ba867ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -111,11 +111,6 @@ ninja.data = [{
           description: "Research on modifying the transition pressure point of Cerium by dopping rare earth metals.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
