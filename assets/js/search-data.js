53326663d70fce1e5333f7ed7a07cdfb5e0fa40f
tttt2342 <37260130+tttt2342@users.noreply.github.com>
2025-01-03 14:04:42 +0000

    Deploying to gh-pages from @ tttt2342/TTSpace@23c3066c7e3677ec55694205c75a7219bf8c31a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-adiabatic-approximation",
+        },{id: "post-scattering",
+      
+        title: "Scattering",
+      
+      description: "Quantum Mechanics Notes",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/scattering/";
+        
+      },
+    },{id: "post-adiabatic-approximation",
       
         title: "Adiabatic Approximation",
       
